78c7260455f895e9804c48dc2c92a89a7ea9942b
Md. Shihab Uddin <programming6090@gmail.com>
2025-09-06 12:13:42 +0600

    Add files via upload

---
+++ Python (zero  to advance)/print_commends_rules.py
@@ -0,0 +1,8 @@
+print("Assalamualaikum\n")
+print("hello world","    ", "i am shihab")
+print("I am Md. Shihab Udiin\t", "my wish AI and Ml topic")
+
+#single line commends
+
+''' multiple line -
+commends rulls. '''
---
+++ Python (zero  to advance)/variable_data_type.py
@@ -0,0 +1,66 @@
+# two string adding rule.
+
+name = "my name is md. shihab uddin"
+wish = "i love allah"
+print(" + ")
+print(name,"",wish)
+print(name + wish)
+
+# string , integer float etc adding system.
+
+name = "Shihab"
+age = 22
+cgpa = 3.83
+print("",name,"\n","age" ,age,"\n","cgpa",cgpa)
+
+# add two numbers (10 + 20 = 30 )
+
+number1 = 10
+number2 = 20
+'''result = number1 + number2
+print("sum of the number is :","",result)'''
+
+print("result =",number1+number2)
+
+# rules of computer memorise:
+
+num1= 50
+print("numb 1=",num1)
+
+num1=30
+num1=35
+print(num1) # here the last declared number is show.
+
+# string and number +,-.*,/ operation is not possible.
+
+# (***) Find out the declared variable type:
+
+s = 79
+print(type(s))
+t = "65"
+print(type(t))
+g = 3.83
+print(type(g))
+
+# some operation of (+,-,*,/,%,**:)
+
+number5 = 5
+number6 = 2
+print(number5 + number6)
+print(number5 - number6)
+print(number5 / number6)
+print(number5 * number6)
+print(number5 % number6)
+print(number5 ** number6)
+print(number5 // number6) #just integer
+
+print(number5 / number6, number5 // number6, number5 % number6)#shortcut
+
+# input() function
+
+name = input("enter anything\n")
+print(name)
+
+print(input("number1") + input("number2"),"=" )
+
+print(input()+input(),"=",)
